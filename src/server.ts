2bbceab825ee1cb3eff25a05b34dbd53763af8e3
Mike Santos <iddeiasimplesmike@gmail.com>
2026-02-19 10:03:59 -0300

    adiciona cors global

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors";
-
+import cors from 'cors';
 
 import { router } from './routes';
 import "./database";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
@@ -23,4 +24,4 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     })
 })
 
-app.listen(process.env.PORT || 3000, () => console.log("Servidor Rodando"))
+app.listen(process.env.PORT || 3000, () => console.log("Servidor Rodando"));
